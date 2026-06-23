3b3004eeb675da058daa13fe1e157c69faf94731
Divyanshu Katiyar <katiyardivyanshu08@gmail.com>
2026-06-24 00:38:56 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (28.36%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -4,19 +4,19 @@ class Solution {
         // binary search
         int st = 0 ;
         int end = n-1;
-        while(st<=end){
-            int mid = st+(end-st)/2;
-            if(nums[mid]==target) return mid;
-            else if (nums[mid]<target)  st=mid+1;
-            else end=mid-1; 
-        }
-        return st;
-        
-        // for( int i=0 ; i<n ; i++){
-        //     if(nums[i]>=target){
-        //         return i ;
-        //     }
+        // while(st<=end){
+        //     int mid = st+(end-st)/2;
+        //     if(nums[mid]==target) return mid;
+        //     else if (nums[mid]<target)  st=mid+1;
+        //     else end=mid-1; 
         // }
-        // return nums.length;   
+        // return st;
+        
+        for( int i=0 ; i<n ; i++){
+            if(nums[i]>=target){
+                return i ;
+            }
+        }
+        return nums.length;   
     }
 }
