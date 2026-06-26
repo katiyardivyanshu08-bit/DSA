0d82fade3829d3a4ea24c6ac953e0d458526ad0c
Divyanshu Katiyar <katiyardivyanshu08@gmail.com>
2026-06-26 11:28:21 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (17.43%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        // s=s.trimmed;
+        int count  = 0;
+        String S = s.trim();
+        int n=S.length();
+        for(int i=n-1 ; i>=0 ; i--){
+
+            if(S.charAt(i)==' '){   //"string" , 'charcter'
+                break;
+            }
+            else count++;
+        }
+        return count;
+    }
+}
