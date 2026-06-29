fef226ed3c2d157d4d01e25bade412fbfea93377
Divyanshu Katiyar <katiyardivyanshu08@gmail.com>
2026-06-29 15:59:31 +0530

    Time: 5 ms (61.84%) | Memory: 44.3 MB (74.64%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,18 +3,17 @@ class Solution {
         int n1=s.length();
         int n2=t.length();
         if(n1!=n2) return false;
+
         int [] arr1 = new int[26];
         int [] arr2 = new int[26];
 
-        for(int i =0 ; i<n1 ; i++){
+        for(int i =0 ; i<n1 ; i++){     // now here n1=n2
             arr1[s.charAt(i)-'a']++;
-        }
-        for(int i =0 ; i<n2 ; i++){
             arr2[t.charAt(i)-'a']++;
         }
-        
+       
         for(int i = 0 ; i<26 ; i++){
-            if(arr1[i]!=arr2[i]){
+            if(arr1[i]!=arr2[i]){      //if(arr1==arr2) it check his address
                 return false ;
             }
         }
