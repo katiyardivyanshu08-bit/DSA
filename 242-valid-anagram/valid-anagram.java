d3a50f6c6595c278d6041826aa842bb084b902e2
Divyanshu Katiyar <katiyardivyanshu08@gmail.com>
2026-07-01 00:10:06 +0530

    Time: 4 ms (82.24%) | Memory: 46.6 MB (16.05%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,30 +1,41 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int n1=s.length();
-        int n2=t.length();
-        if(n1!=n2) return false;
+    //     int n1=s.length();
+    //     int n2=t.length();
+    //     if(n1!=n2) return false;
 
-        int [] arr1 = new int[26];
-        int [] arr2 = new int[26];
+    //     int [] arr1 = new int[26];
+    //     int [] arr2 = new int[26];
 
-        for(int i =0 ; i<n1 ; i++){     // now here n1=n2
-            arr1[s.charAt(i)-'a']++;
-            arr2[t.charAt(i)-'a']++;
-        }
+    //     for(int i =0 ; i<n1 ; i++){     // now here n1=n2
+    //         arr1[s.charAt(i)-'a']++;
+    //         arr2[t.charAt(i)-'a']++;
+    //     }
 
-        //  for(int i =0 ; i<n1 ; i++){
-        //     arr1[s.charAt(i)-'a']++;
-        // }
-        // for(int i =0 ; i<n2 ; i++){
-        //     arr2[t.charAt(i)-'a']++;
-        // }
+    //     //  for(int i =0 ; i<n1 ; i++){
+    //     //     arr1[s.charAt(i)-'a']++;
+    //     // }
+    //     // for(int i =0 ; i<n2 ; i++){
+    //     //     arr2[t.charAt(i)-'a']++;
+    //     // }
+        
        
-        for(int i = 0 ; i<26 ; i++){
-            if(arr1[i]!=arr2[i]){      //if(arr1==arr2) it check his address
-                return false ;
-            }
+    //     for(int i = 0 ; i<26 ; i++){
+    //         if(arr1[i]!=arr2[i]){      //if(arr1==arr2) it check his address
+    //             return false ;
+    //         }
+    //     }
+    //     return true ;
+
+    if(s.length()!=t.length()) return false;
+        char[] arr1 = s.toCharArray();
+        char[] arr2 = t.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        for(int i=0;i<arr1.length;i++){
+            if(arr1[i]!=arr2[i]) return false;
         }
-        return true ;
-    }
+        return true;
+     }
 }
 
