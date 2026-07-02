374c128d83cef840730ce31fa966133104259f82
Divyanshu Katiyar <katiyardivyanshu08@gmail.com>
2026-07-03 02:30:20 +0530

    Time: 14 ms (32.26%) | Memory: 46.4 MB (24.51%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s==" ") return true;
+        s=s.toLowerCase();
+        s=s.replaceAll("[^a-z0-9]","");
+        StringBuilder t = new StringBuilder(s);
+        t.reverse();
+        String x=t.toString();
+        if(s.equals(x)) return true;
+        else return false;
+
+    }
+}
