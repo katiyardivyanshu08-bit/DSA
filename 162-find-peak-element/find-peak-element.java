6f80462beb950cddc5445300fbeb7b889e901c85
Divyanshu Katiyar <katiyardivyanshu08@gmail.com>
2026-07-05 21:24:48 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (5.29%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -8,7 +8,6 @@ class Solution {
     //     int 
     //     if(nums[i]>)
     // }
-    // int i ;
     for(int i =0 ; i<n ; i++){
         if((i==0 || nums[i]>nums[i-1]) && (i==n-1 || nums[i]>nums[i+1])) {
             return i ;
