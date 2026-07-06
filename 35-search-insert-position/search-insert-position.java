d5f04f7081672d7a06d79d63fb2b1d32c344b2fb
Divyanshu Katiyar <katiyardivyanshu08@gmail.com>
2026-07-06 17:16:29 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (28.60%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -7,7 +7,7 @@ class Solution {
         while(st<=end){
             int mid = st+(end-st)/2;
             if(nums[mid]==target) return mid;
-            else if (nums[mid]<target)  st=mid+1;
+            else if (nums[mid]<target)  st=mid+1; // insert first occu (=) insert last occu
             else end=mid-1; 
         }
         return st;
