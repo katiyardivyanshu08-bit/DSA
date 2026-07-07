b19c7e80a8ce32573d4a53c3a7c5e17ca5d72fea
Divyanshu Katiyar <katiyardivyanshu08@gmail.com>
2026-07-07 19:15:36 +0530

    Time: 1 ms (100.00%) | Memory: 42.8 MB (43.85%) - LeetSync

--- 441-arranging-coins/arranging-coins.java
+++ 441-arranging-coins/arranging-coins.java
@@ -1,17 +1,34 @@
 class Solution {
-     public int mySqrt(long x) {
-        long st =1;
-        long end = x;
-        while(st<=end){
-            long mid=st+(end-st)/2;
-            if(mid == x/mid) return (int) mid;  //mid*mid==x
-            else if(mid > x/mid) end=mid-1;
-            else st=mid+1;
+     public int arrangeCoins(int n) {
+         long lo = 0, hi = n, ans = 0;
+        while(lo<=hi){
+            long k = lo + (hi-lo)/2;
+            long m = k*(k+1)/2;
+            if(m==n) return (int)k;
+            else if(m>n){
+                
+                hi = k - 1;
+            }
+            else {
+                ans = k;
+                lo = k + 1;
+            }
         }
-        return (int) end ;
-     }
-    public int arrangeCoins(int n) {
-        long m= n;                        // long m = (long) n
-        return (mySqrt(8*m+1)-1)/2;
+        return (int)(ans); 
+    //or
+    //  public int mySqrt(long x) {
+    //     long st =1;
+    //     long end = x;
+    //     while(st<=end){
+    //         long mid=st+(end-st)/2;
+    //         if(mid == x/mid) return (int) mid;  //mid*mid==x
+    //         else if(mid > x/mid) end=mid-1;
+    //         else st=mid+1;
+    //     }
+    //     return (int) end ;
+    //  }
+    // public int arrangeCoins(int n) {
+    //     long m= n;                        // long m = (long) n
+    //     return (mySqrt(8*m+1)-1)/2;
     }
 }
