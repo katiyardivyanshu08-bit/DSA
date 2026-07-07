6c073f1610e22ee395b42c64b6026fc11dad1be0
Divyanshu Katiyar <katiyardivyanshu08@gmail.com>
2026-07-07 22:24:24 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (24.25%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -6,12 +6,12 @@ class Solution {
         int lo = 0, hi = r*c-1;
         while(lo<=hi){
             int mid = (lo+hi)/2;
-            int midRow = mid/c, midCol = mid%c;
+            int midRow = mid/c, midCol = mid%c;  //?
             if(matrix[midRow][midCol]==target) return true;
             else if(matrix[midRow][midCol]>target) hi = mid - 1;
             else lo = mid + 1;
         }
-        
+
         //or
         // for(int i =0 ; i<r ; i++ ){
         //     for(int j =0 ; j<c; j++){
