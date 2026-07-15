3b4d4adde79fa9384a53d30222d37b79e84bb85c
Divyanshu Katiyar <katiyardivyanshu08@gmail.com>
2026-07-15 16:34:30 +0530

    Time: 309 ms (5.27%) | Memory: 81.4 MB (15.26%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,7 +1,23 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
+        Arrays.sort(nums);
+        List<Integer> ans=new ArrayList<>();
+        for(int x : nums) if(x>0) ans.add(x);
+
+        for(int i=1;i<ans.size();){
+            if(ans.get(i).equals(ans.get(i-1))) ans.remove(i);
+            else i++;
+        }
+          // System.out.print(ans);
+        int y=1;
+        for(int x=0 ; x<ans.size() ; x++){
+            if(ans.get(x)!=y) return y;
+            y++;
+        }
+        return ans.size()+1;
+
         // cyclic sort
-        int n=nums.length;
+        // int n=nums.length;
         // int i=0;
         // while(i<n){
         //     int correct=nums[i]; // including 0 and nums[i]-1 for st from 1 
@@ -12,24 +28,6 @@ class Solution {
         //     }
         //     else i++;
         // }
-        Arrays.sort(nums);
-        List<Integer> ans=new ArrayList<>();
-        for(int x : nums){
-            if(x>0) ans.add(x);
-        }
-        for(int i=1;i<ans.size();){
-    if(ans.get(i).equals(ans.get(i-1))) ans.remove(i);
-    else i++;
-}
-       System.out.print(ans);
-            int y=1;
-        for(int x=0 ; x<ans.size() ; x++){
-            if(ans.get(x)!=y){
-                return y;
-                
-            }
-            y++;
-        }
-        return ans.size()+1;
+
     }
 }
