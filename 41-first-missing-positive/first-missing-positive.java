f0105dce25fa0845d2e606d14d2aa01d5e0aca26
Divyanshu Katiyar <katiyardivyanshu08@gmail.com>
2026-07-15 21:43:07 +0530

    Time: 1 ms (100.00%) | Memory: 71.1 MB (85.68%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,33 +1,38 @@
 class Solution {
-    public int firstMissingPositive(int[] nums) {
-        Arrays.sort(nums);
-        List<Integer> ans=new ArrayList<>();
-        for(int x : nums) if(x>0) ans.add(x);
-
-        for(int i=1;i<ans.size();){
-            if(ans.get(i).equals(ans.get(i-1))) ans.remove(i);
-            else i++;
-        }
-          // System.out.print(ans);
-        int y=1;
-        for(int x=0 ; x<ans.size() ; x++){
-            if(ans.get(x)!=y) return y;
-            y++;
-        }
-        return ans.size()+1;
-
         // cyclic sort
-        // int n=nums.length;
-        // int i=0;
-        // while(i<n){
-        //     int correct=nums[i]; // including 0 and nums[i]-1 for st from 1 
-        //     if(nums[i]!=nums[correct]){
-        //         int temp=nums[i];
-        //         nums[i]=nums[correct];
-        //         nums[correct]=temp;
-        //     }
-        //     else i++;
-        // }
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public int firstMissingPositive(int[] nums) {
+        int i = 0, n = nums.length; // 1 to n
+        while(i<n){
+            if(nums[i]<=0) i++;
+            else if(nums[i]>n) i++;
+            else if(nums[i]==i+1) i++; // already at correct place
+            else if(nums[i]==nums[nums[i]-1]) i++; // +ve duplicate
+            else swap(nums,i,nums[i]-1);
+        }
+        for(i=0;i<n;i++){
+            if(nums[i] != i+1) return i+1;
+        }
+        return n+1;    
 
+    //     numsays.sort(nums);
+    //     List<Integer> ans=new numsayList<>();
+    //     for(int x : nums) if(x>0) ans.add(x);
+
+    //     for(int i=1;i<ans.size();){
+    //         if(ans.get(i).equals(ans.get(i-1))) ans.remove(i);
+    //         else i++;
+    //     }
+    //       // System.out.print(ans);
+    //     int y=1;
+    //     for(int x=0 ; x<ans.size() ; x++){
+    //         if(ans.get(x)!=y) return y;
+    //         y++;
+    //     }
+    //     return ans.size()+1;
     }
 }
