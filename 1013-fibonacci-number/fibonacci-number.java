bd131984fbaaa90c4187be9d46e5639c6d3c147c
Divyanshu Katiyar <katiyardivyanshu08@gmail.com>
2026-07-31 20:09:16 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (16.03%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,8 +1,18 @@
 class Solution {
     public int fib(int n) {
-        if(n==0 || n==1) return n;
-        return fib(n-1)+fib(n-2);  // O(2powN)
+        if(n<=1) return n;
+        // if(n==0 || n==1) return n;
+        // return fib(n-1)+fib(n-2);  // O(2powN)
 
+        int sum=0;
+        int f=0;
+        int s=1;
+        for(int i=2;i<=n;i++){
+            sum=f+s;
+            f=s;
+            s=sum;
+        }
+        return sum;
 
     }
 }
